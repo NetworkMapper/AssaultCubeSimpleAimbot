17b5b77be6399f3272e7713556d7fe25f96920c9
brutalhomeattacks <176590097+brutalhomeattacks@users.noreply.github.com>
2024-08-04 01:22:21 +0300

    Update Memory.hpp

--- AssaultCube [AIMBOT]/Simple Aimbot/Memory.hpp
+++ AssaultCube [AIMBOT]/Simple Aimbot/Memory.hpp
@@ -1,18 +1,18 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 #include <tchar.h>
+#include <optional>
+
 
 namespace Mem {
-	HANDLE GetProcessHandle(const TCHAR* name) {
+	HANDLE GetProcessHandle(const CHAR* name) {
 
-		PROCESSENTRY32 entry{ };
-		entry.dwSize = sizeof(PROCESSENTRY32);
-
+		PROCESSENTRY32 entry{ entry.dwSize = sizeof(PROCESSENTRY32) };
 		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 		if (Process32First(snapshot, &entry)) {
 			do {
-				if (_tcscmp(entry.szExeFile, name) == 0) {
+				if (strcmp(entry.szExeFile, name) == 0) {
 					return OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
 				}
 			} while (Process32Next(snapshot, &entry));
@@ -20,15 +20,14 @@ namespace Mem {
 
 		return nullptr;
 	}
-	DWORD GetProcessId(const TCHAR* name) {
-		PROCESSENTRY32 entry{ };
-		entry.dwSize = sizeof(PROCESSENTRY32);
+	std::optional<uint32_t> GetProcessId(const CHAR* name) {
+		PROCESSENTRY32 entry{ entry.dwSize = sizeof(PROCESSENTRY32) };
 
-		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 		if (Process32First(snapshot, &entry)) {
 			do {
-				if (_tcscmp(entry.szExeFile, name) == 0) {
+				if (strcmp(entry.szExeFile, name) == 0) {
 					return entry.th32ProcessID;
 				}
 			} while (Process32Next(snapshot, &entry));
@@ -36,20 +35,19 @@ namespace Mem {
 
 		return 0;
 	}
-	uintptr_t GetModuleBase(DWORD pid, const TCHAR* name) {
-		MODULEENTRY32 entry{ };
-		entry.dwSize = sizeof(MODULEENTRY32);
+	std::optional<uintptr_t> GetModuleBase(uint32_t pid, const CHAR* name) {
+		MODULEENTRY32 entry{ entry.dwSize = sizeof(MODULEENTRY32) };
 
-		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32, pid);
+		auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE32, pid);
 
 		if (Module32First(snapshot, &entry)) {
 			do {
-				if (_tcscmp(entry.szModule, name) == 0) {
+				if (strcmp(entry.szModule, name) == 0) {
 					return reinterpret_cast<uintptr_t>(entry.modBaseAddr);
 				}
 			} while (Module32Next(snapshot, &entry));
 		}
 
-		return 0;
+		return NULL;
 	}
 }
